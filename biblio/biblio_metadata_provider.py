f08a53be9ccc5946614231265ffa7a246430ad2c
Ondrej Kosarko <ko_ok@centrum.cz>
2016-08-19 17:31:52 +0200

    mapped few more fields, read the comments

--- biblio/biblio_metadata_provider.py
+++ biblio/biblio_metadata_provider.py
@@ -4,6 +4,7 @@
 import os
 import logging
 import xml.etree.ElementTree
+from xml.sax.saxutils import escape
 
 _logger = logging.getLogger()
 
@@ -15,14 +16,14 @@ class base_biblio(object):
         self._parse(ftor)
 
     def _parse(self, ftor):
-        root = xml.etree.ElementTree.parse( self.f ).getroot()
-        for elem in root.findall( 'Record' ):
+        root = xml.etree.ElementTree.parse(self.f).getroot()
+        for elem in root.findall('Record'):
             if ftor is None:
                 self.ids[elem.get('Id')] = {}
             else:
                 v = ftor(elem)
                 if v is not None:
-                    self.ids[elem.get( 'Id' )] = v
+                    self.ids[elem.get('Id')] = v
         _logger.info("Found [%d] entries in [%s]", len(self.ids), self.f)
 
 
@@ -30,7 +31,7 @@ class authors(base_biblio):
     def __init__(self, f):
         def format_names(elem):
             d = dict((x.get("Label"), x.text) for x in elem)
-            #last, first for now check with https://guidelines.openaire.eu/en/latest/literature/field_creator.html#dc-creator
+            #XXX: last, first for now check with https://guidelines.openaire.eu/en/latest/literature/field_creator.html#dc-creator
             first = d.get('First name')
             last = d.get('Last name')
             if first == last == None:
@@ -44,36 +45,79 @@ class authors(base_biblio):
             if first is not None:
                 ret += first
             return ret
-        super( authors, self ).__init__( f, format_names )
+
+        super(authors, self).__init__(f, format_names)
 
 
 class grants(base_biblio):
-    supported = ("EU", )
+    supported = ("EU",)
 
-    def __init__(self, f):
+    def __init__(self, f, openaire):
         def filter_ids(elem):
             d = dict((x.get("Label"), x.text) for x in elem)
-            return d if d.get("Agency", "") in grants.supported \
-                else None
-        super( grants, self ).__init__( f, filter_ids )
+            if d.get("Agency", "") in grants.supported:
+                code = d.get("Code")
+                d["openaire_id"] = self._get_openaire_id(code, openaire)
+                return d
 
+            return None
+
+        super(grants, self).__init__(f, filter_ids)
+
+    def _get_openaire_id(self, code, openaire):
+        if code is None:
+            return None
+        ids = code.split('-')
+        if ids[-1].isdigit():
+            id = ids[-1]
+        elif len(ids) > 1 and ids[-2].isdigit():
+            id = ids[-2]
+        else:
+            id = None
+        if id is None:
+            _logger.error("Failed to parse grant code %s", code)
+            return None
+        results = [];
+        for openaire_id in openaire.ids.keys():
+            if id in openaire_id:
+                results.append(openaire_id)
+
+        if len(results) == 1:
+            return results[0]
+        else:
+            _logger.error("Failed to uniquely identify openaire project from code %s", code)
+            return None
 
 class publications(base_biblio):
     def __init__(self, f, grants):
-        ids = set(grants.ids.keys())
+        grant_ids = set(grants.ids.keys())
 
         def filter_ids(elem):
             d = dict((x.get("Label"), x.text) for x in elem)
             supported = d.get("Supported by", "") or ""
             auths = d.get("Author(s)", "") or ""
-            d["Supported by"] = set(supported.split(";"))
+            d["Supported by"] = set(supported.split(";")) & grant_ids
             d["Author(s)"] = set(auths.split(";"))
-            return d if 0 < len(d["Supported by"] & ids) else None
-        super( publications, self ).__init__( f, filter_ids )
+            return d if 0 < len(d["Supported by"]) else None
+
+        super(publications, self).__init__(f, filter_ids)
+
+
+class Openaire(object):
+    def __init__(self, f):
+        self.f = f
+        self.ids = {}
+        self._parse()
+
+    def _parse(self):
+        root = xml.etree.ElementTree.parse(self.f).getroot()
+        for elem in root.findall('.//pair'):
+            id = elem.find("stored-value").text
+            self.ids[id] = {}
+        _logger.info("Found [%d] entries in [%s]", len(self.ids), self.f)
 
 
 class biblio(object):
-
     dc_template = u"""
 <oai_dc:dc xmlns:oai_dc="http://www.openarchives.org/OAI/2.0/oai_dc/" xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance" xmlns:dc="http://purl.org/dc/elements/1.1/" xsi:schemaLocation="http://www.openarchives.org/OAI/2.0/oai_dc/ http://www.openarchives.org/OAI/2.0/oai_dc.xsd">
 %s
@@ -81,39 +125,86 @@ class biblio(object):
 """
 
     def __init__(self, d):
+        # List of openaire projects as used and fetched by dspace. [dspace]/config/openaire-cache.list
+        self.openaire = Openaire(os.path.join(d, "openaire-cache.list"))
         self.authors = authors(os.path.join(d, "authors.xml"))
-        self.grants = grants( os.path.join( d, "grants.xml" ) )
-        self.publications = publications( os.path.join( d, "publications.xml" ), self.grants )
+        self.grants = grants(os.path.join(d, "grants.xml"), self.openaire)
+        self.publications = publications(os.path.join(d, "publications.xml"), self.grants)
         self.identifiers = self.publications.ids
 
     def to_dc(self, identifier):
         rec = self.publications.ids[identifier]
-        #Mandatory fields
-        dc_title = '\n'.join([u"<dc:title>%s</dc:title>" % rec[title_field] if rec[title_field] is not None else ""
+        # Mandatory fields
+        dc_title = '\n'.join([u"<dc:title>%s</dc:title>" % escape(rec[title_field]) if rec[title_field] is not None else ""
                               for title_field in ["Title", "Original title", "English title", "Czech title"]])
         dc_creators = ""
         try:
-            dc_creators = u"\n".join([ u"<dc:creator>%s</dc:creator>" % self.authors.ids[author_id] for author_id in rec.get("Author(s)")])
+            dc_creators = u"\n".join([u"<dc:creator>%s</dc:creator>" % escape(self.authors.ids[author_id])
+                                      for author_id in rec.get("Author(s)")])
         except KeyError:
             _logger.error("Error fetching authors for %s", identifier)
-#        # Project identifier info:eu-repo/grantAgreement
-#        dc_relation =
-#        #Access level info:eu-repo/semantics
-#        dc_rights
-#        #XXX: subject/keywords nothing to map?
-#        #dc_subject
-#        #EnglishAbstract
-#        dc_description
-#        #Publisher
-#        dc_publisher
-#        #publication date
-#        #Use Year for now, there is also a date field
-#        dc_date
-#        #publication type info:eu-repo/semantics/
-#        dc_type
-#        #XXX: resource identifier, (URL,DOI,URN:NBN,ISBN,ISSN,etc.), do we put there biblio URL, if URL is empty?
-#        dc_identifier
-        dc_metadata = dc_title + '\n' + dc_creators
+        # Project identifier info:eu-repo/grantAgreement
+        dc_relations = ''
+        try:
+            dc_relations = u"\n".join([u"<dc:relation>%s</dc:relation>" % self.grants.ids[grant_id]["openaire_id"]
+                                      for grant_id in rec.get("Supported by")])
+        except KeyError:
+            _logger.error("Error fetching openaire_id for %s", identifier)
+        #Access level info:eu-repo/semantics
+        #XXX: hardcoded openaccess
+        dc_rights = u"<dc:rights>%s</dc:rights>" % 'info:eu-repo/semantics/openAccess'
+        #XXX: subject/keywords nothing to map?
+        #        #dc_subject
+        #description...use english abstract
+        dc_description = u"<dc:description>%s</dc:description>" % escape(rec.get("English abstract")) if rec.get("English abstract") is not None else ""
+        #Publisher
+        dc_publisher = u"<dc:publisher>%s</dc:publisher>" % escape(rec.get("Publisher")) if rec.get("Publisher") is not None else ""
+        #publication date #Use Year for now, there is also a date field
+        dc_date = u"<dc:date>%s</dc:date>" % rec.get("Year") if rec.get("Year") is not None else ""
+        #publication type info:eu-repo/semantics/
+        #XXX come up with a mapping from
+        # 630 Inproceedings
+        # 460 InProceedingsWithoutISBN
+        # 274 OralPresentation
+        # 241 Article
+        # 192 DataSW
+        # 83 Inbook
+        # 62 Techreport
+        # 48 PosterAbstractDemo
+        # 34 Book
+        # 31 Phdthesis
+        # 24 Review
+        # 18 Prototype
+        # 16 Mastersthesis
+        # 15 Popular
+        # 4 Electronic
+        # 2 LectureNotes
+        # 2 Habilitation
+        # 1 Survey
+        # 1 Proceedings
+        # 1 ArticleTranslation
+        #        TO
+        #        info:eu - repo / semantics / article
+        #        info:eu - repo / semantics / bachelorThesis
+        #        info:eu - repo / semantics / masterThesis
+        #        info:eu - repo / semantics / doctoralThesis
+        #        info:eu - repo / semantics / book
+        #        info:eu - repo / semantics / bookPart
+        #        info:eu - repo / semantics / review
+        #        info:eu - repo / semantics / conferenceObject
+        #        info:eu - repo / semantics / lecture
+        #        info:eu - repo / semantics / workingPaper
+        #        info:eu - repo / semantics / preprint
+        #        info:eu - repo / semantics / report
+        #        info:eu - repo / semantics / annotation
+        #        info:eu - repo / semantics / contributionToPeriodical
+        #        info:eu - repo / semantics / patent
+        #        info:eu - repo / semantics / other
+        #        dc_type
+        #XXX: resource identifier, (URL,DOI,URN:NBN,ISBN,ISSN,etc.), do we put there biblio URL, if URL is empty, can we get more details for InProceedings?
+        #        dc_identifier
+        #XXX Make sure the values in those fields are properly escaped!
+        dc_metadata = '\n'.join([dc_title, dc_creators, dc_relations, dc_rights, dc_description, dc_publisher, dc_date])
         return biblio.dc_template % dc_metadata
 
 
@@ -206,7 +297,7 @@ class Provider(object):
             `a:b` and `a` must also be included).
         """
         # This provider does not use sets.
-        return [ ("openaire", "openaire") ]
+        return [("openaire", "openaire")]
 
     def get_record(self, identifier, metadata_prefix):
         """

1a2baaff51b9a9c1b6eec0cfd7186487e8dfcc57
root <root@lindat.mff.cuni.cz>
2016-12-20 23:31:47 +0100

    update script to handle encodings

--- biblio/update.sh
+++ biblio/update.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 # - you should have biblio checkedout to biblio_from_svn and symlinked input/ to biblio_from_svn/xmldump
+export LC_CTYPE=en_US.UTF-8
 pushd biblio_from_svn
 svn update
 popd
